0d2082abe616a899cad892e374706303ae01f5cd
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2020-05-29 21:11:26 +0200

    Initial files

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<?php get_header(); ?>
+    <main role="main">
+        <?php if ( have_posts() ) {
+            /* Start the Loop */
+            while ( have_posts() ) {
+                the_post();
+                get_template_part( 'content', get_post_format() );
+            }
+        }
+        the_posts_pagination( array( 'type' => 'list' ) ); ?>
+    </main>
+<?php get_sidebar();
+get_footer(); ?>
+</html>
