d2200d7516387ccb12de8d0d25932c72b9d2c7d2
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2020-08-24 20:33:51 +0200

    New template for landing page

---
+++ fullwidth-notitle.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Template Name: Komplette Breite
+ */
+
+global $no_container;
+$no_container = True; // leave out containers in header and footer section
+
+get_header(); ?>
+
+	<section id="full-width" >
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', 'notitle' );
+
+		endwhile; // End of the loop.
+		?>
+	</section><!-- #primary -->
+
+<?php
+get_footer();
